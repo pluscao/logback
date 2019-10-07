a495dfc326b0a8f426cdd8dd819b8abe5e99cdd4
silveryC <hello.shengC@gmail.com>
2019-10-07 15:29:47 +0800

    .

---
+++ logback-read-debug/src/main/java/ch/qos/logback/App.java
@@ -0,0 +1,13 @@
+package ch.qos.logback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
